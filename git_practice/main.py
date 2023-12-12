6647c9a1c2929ece9bdfc0857dbc8a3d38b564cc
leesm123 <151709573+leesm123@users.noreply.github.com>
2023-12-12 09:14:46 +0900

    Update main.py

--- git_practice/main.py
+++ git_practice/main.py
@@ -1,10 +1,8 @@
-# main.py
-import list_ops as lo
-
-if __name__ == "__main__":
-    foo = [1,2,3]
-    bar = [4,5,6]
-    print("foo+bar=", lo.add(foo, bar))
-    print("foo-bar=", lo.subtract(foo, bar))
-    print("foo*bar=", lo.multiply(foo, bar))
-    print("foo/bar=", lo.divide(foo, bar))
+int main(int argc, char* argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<SelfDrive>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
